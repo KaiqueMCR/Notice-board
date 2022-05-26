04186efa55d174e92dd48dde142d0fbb33465077
KaiqueMCR <kaiquedev404@gmail.com>
2022-05-26 14:44:31 -0300

    fix production link on script

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,5 +1,5 @@
 const URL = `https://notice-board-production.up.railway.app/api/all`
-const postURL = 'https://notice-board-production.up.railway.app/newPost'
+const postURL = 'https://notice-board-production.up.railway.app/api/newPost'
 const deleteURL =
   'https://notice-board-production.up.railway.app/api/deletePost'
 
