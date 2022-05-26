79af2494a1ca4c4695161d439ad07d44892f482c
KaiqueMCR <kaiquedev404@gmail.com>
2022-05-26 13:19:20 -0300

    add use routes

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+const apiRoute = require('./routes/api')
 
 app.use(express.json())
+app.use('/api', apiRoute)
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(process.env.PORT || 8080, () => {
   console.log('HTTP server ruinning')
