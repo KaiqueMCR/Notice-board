3ff185eba7d2d343b3742eb0ed000ef022830299
KaiqueMCR <kaiquedev404@gmail.com>
2022-05-27 21:48:32 -0300

    more fixes

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const apiRoute = require('./routes/api')
 
 app.use(express.json())
 app.use('/api', apiRoute)
-app.use(express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.listen(process.env.PORT || 8080, () => {
-  console.log('HTTP server ruinning')
+  console.log('HTTP server running')
 })
